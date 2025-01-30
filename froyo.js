de5ae2298f432085382ee682f144035b23cac6d6
YourName <your_email@example.com>
2025-01-30 01:09:48 -0500

    object created and returned

--- froyo.js
+++ froyo.js
@@ -1,9 +1,16 @@
-const key = prompt("Welcome! Please, enter list of ice cream flavors.");
+const froyo = prompt("Welcome! Please, enter list of ice cream flavors.");
+const flavors = froyo.split(",");
 
-let flavors = "vanilla,strawberry,coffee";
-const froyo = flavors.split(",");
-console.log(flavors);
-
-for (let f of froyo) {
-  console.log(f);
+function getFlavors(flavors) {
+  const order = {};
+  for (let f of flavors) {
+    if (order[f]) {
+      order[f]++;
+    } else {
+      order[f] = 1;
+    }
+  }
+  return order;
 }
+
+console.log(getFlavors(flavors));
